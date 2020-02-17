6253b5ec379e104812ff7dfa6c80be2b21194aa4
taipeiyuka <55477647+taipeiyuka@users.noreply.github.com>
2020-02-17 16:43:16 +0800

    keyboard controlling ball
    The assets include the background and a ball that can only be controlled by keyboard. Also the program "Globals.cs" can take input from serial port.

---
+++ Assets/Globals.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System.IO.Ports;
+
+
+public class Globals : MonoBehaviour
+{
+    public SerialPort mySerialPort;
+    public string com = "COM4";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySerialPort = new SerialPort(com);
+
+        mySerialPort.BaudRate = 9600;
+        mySerialPort.Parity = Parity.None;
+        mySerialPort.StopBits = StopBits.One;
+        mySerialPort.DataBits = 8;
+        mySerialPort.Handshake = Handshake.None;
+        mySerialPort.RtsEnable = true;
+        mySerialPort.ReadTimeout = 1;
+
+        mySerialPort.Open();
+
+        //mySerialPort.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value;
+        char[] val = new char[10];
+        float v;
+        mySerialPort.Read(val, 0, 7);
+        value = val.ToString();
+        Debug.Log(value);
+
+    }
+}
---
+++ Assets/KeyboardBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.position -= new Vector3(0.1f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 50f, ForceMode2D.Force);
+            //ForceMode2D.Impulse
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position -= new Vector3(0, 0.1f, 0);
+        }
+    }
+}
